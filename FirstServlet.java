a82cca671e93355df9e0aefcf818a98c3a40e241
ZkhHelen <126550759+ZkhHelen@users.noreply.github.com>
2023-04-13 14:51:09 +0300

    Add files via upload

--- FirstServlet.java
+++ FirstServlet.java
@@ -1,4 +1,5 @@
 
+import Service.CheckService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,5 +15,19 @@ public class FirstServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1> First Servlet </h1>");
         out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CheckService checkService = new CheckService();
+        String username= req.getParameter( "username");
+        String password= req.getParameter( "password");
+        resp.setContentType("text/html");
+        PrintWriter out=resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h2> POST </h2>");
+        out.println("<h1> "+ checkService.addString(username)+"</h1>");
+        out.println("</body></html>");
+
     }
 }
